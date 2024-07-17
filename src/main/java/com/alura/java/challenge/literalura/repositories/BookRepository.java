fe33c0e182e93dfb36dd1f4ef6ebf1c4de311e35
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-16 23:43:55 -0300

    update book repo

--- src/main/java/com/alura/java/challenge/literalura/repositories/BookRepository.java
+++ src/main/java/com/alura/java/challenge/literalura/repositories/BookRepository.java
@@ -1,8 +1,8 @@
-package com.alura_oracle_one.GutendexApplication.repositories;
+package com.alura.java.challenge.literalura.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.alura_oracle_one.GutendexApplication.models.Book;
+import com.alura.java.challenge.literalura.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByTitle(String title);
