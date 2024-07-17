a46ca0f6c897aafc99394264febb4f8421e8b1fa
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-16 23:44:38 -0300

    update gutendex app

--- src/main/java/com/alura/java/challenge/literalura/GutendexApplication.java
+++ src/main/java/com/alura/java/challenge/literalura/GutendexApplication.java
@@ -1,4 +1,4 @@
-package com.alura_oracle_one.GutendexApplication;
+package com.alura.java.challenge.literalura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
