a03ff4026358d711aee80bd8022c0e0054283946
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-16 23:48:03 -0300

    rename main

--- src/main/java/com/alura/java/challenge/literalura/main.java
+++ src/main/java/com/alura/java/challenge/literalura/main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GutendexApplication {
+public class main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GutendexApplication.class, args);
+		SpringApplication.run(main.class, args);
 	}
 
 }
